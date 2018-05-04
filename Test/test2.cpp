103efa86cd5e9c9bfd07ae6e0838515e1be01d77
Rouder <1158261439@qq.com>
2018-05-04 16:59:22 +0800

    update

--- Test/test2.cpp
+++ Test/test2.cpp
@@ -1,4 +1,4 @@
-﻿#if 0
+﻿#if 1
 
 #include <iostream>
 using namespace std;
@@ -6,7 +6,7 @@ using namespace std;
 class class0
 {
 public:
-	void test()   
+	void test()		//加上virtual后，指针有点不同
 	{
 		cout << "0" << endl;
 		cout << this << endl;
@@ -45,8 +45,8 @@ public:
 };
 
 
-//结论：只要是指针调用函数，都需要判断强转后类型和原类型来确定调用的虚函数;指针型调用+虚函数才有多态效果
-//		调用继承下来的函数(包括普通函数和虚函数（test3中证明）)，只是简单链接到父类那边的函数去执行，这时这个函数中的this是父类的类型
+//结论：只要是指针调用函数，都需要判断静态类型和动态类型来确定调用的虚函数;指针型调用+虚函数才有多态效果
+//		调用函数时，this满足"一链同针"、"异链异针"
 int main()
 {
 	//不要被下面的代码迷惑了，下面根本没有用到多态。自己拥有的函数是什么就调用什么
@@ -58,15 +58,14 @@ int main()
 // 	A.call();
 // 	A.test();
 
-	//A1和A2在call之后都值调用到class0的test是因为classA的call函数是在来自class0的，调用时A1的类型会被强转成了class0，但是class0中的test还不是虚函数，所以多态没有实现
+	//A1和A2在call之后都只调用到class0的test是因为classA的call函数是在来自class0的，调用时A1的类型会被强转成了class0，但是class0中的test还不是虚函数，所以多态没有实现
 	classA * A1 = new classA();
 	A1->call();                                  
 	A1->test();
 
-	classA * A2 = new classB();
-	A2->call();
-	A2->test();
-
+	//classA * A2 = new classB();
+	//A2->call();
+	//A2->test();
 
 	cin.get();
 	return 0;

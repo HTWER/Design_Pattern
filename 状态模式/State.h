409fbaddd40652b6b7f629ddd2246fa17a8f6ad6
HTWER <1158261439@qq.com>
2019-01-26 17:56:19 +0800

    update

--- 状态模式/State.h
+++ 状态模式/State.h
@@ -8,6 +8,7 @@ class State
 public:
 	virtual void Enter(StateMachine* stateMachine){};
 	virtual void Exit(StateMachine* stateMachine){};
+	//以下纯虚函数根据业务情况来定
 	virtual void insertQuarter(StateMachine* stateMachine) = 0;
 	virtual void ejectQuarter(StateMachine* stateMachine) = 0;
 	virtual void turnCrank(StateMachine* stateMachine) = 0;
